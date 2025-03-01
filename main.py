aa7a5d3598f7294051d38dbd256f9e92d28e8667
KhesehangLimbu <khesehang81@gmail.com>
2025-03-02 00:50:04 +0545

    Dictionary: First

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 """
-Double the String!
-The fantasy quest team wants to add a new easter egg potion to Fantasy Quest! It causes characters to have their in-game chat manipulated when they send messages. The potion doubles every character they send!
+Dictionaries
+Dictionaries in Python are used to store data values in key -> value pairs. Dictionaries are a great way to store groups of information.
 
-The message they type: Hello there
-The message that is sent: HHeelllloo  tthheerree
+# use curly braces
+# add key-value pairs
+car = {
+  "brand": "Toyota",
+  "model": "Camry",
+  "year": 2019
+}
+
+Here the car variable is assigned to a dictionary {} containing the keys brand, model and year. The keys' corresponding values are Toyota, Camry and 2019.
+
 Assignment
-Complete the double_string function. It takes a string as input and returns a "doubled" version, including spaces!
+Complete the get_character_record function. It takes a character's name, server, level, and rank as individual inputs, and returns a dictionary with the following string keys:
 
-Example output
+"name"
+"server"
+"level"
+"rank"
+"id"
+Create and return a dictionary with the keys above. Assign each of the four inputs to the matching key, ie: "name": name.
 
-sentence = "LF3M BRD full clear"
-print(double_string(sentence)) # "LLFF33MM  BBRRDD  ffuullll  cclleeaarr"
+Next, we can't have two characters named bloodwarrior123's on the same server! For the fifth key, id, create a unique value as follows:
+
+Concatenate the name and the server inputs with a # in the middle. For example, given:
+
+name = "bloodwarrior123"
+server = "server1"
+Then the id field would be set to bloodwarrior123#server1.
 """
-
-def double_string(string):
-    double_string = ""
-    for char in string:
-        double_string += char + char
-    return double_string
-
+def get_character_record(name, server, level, rank):
+    return {
+        "name": name,
+        "server": server,
+        "level": level,
+        "rank": rank,
+        "id": f"{name}#{server}"
+    }
 
