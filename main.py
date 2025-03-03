c79f9dec680f3ad8c495bcd3326e969fcc9a9bce
KhesehangLimbu <khesehang81@gmail.com>
2025-03-04 00:12:17 +0545

    Challenges: 1

--- main.py
+++ main.py
@@ -1,61 +1,18 @@
 """
-Purchasing
-Now we need to use our purchase_item function to process an entire list of character purchases.
+Challenges
+In this chapter we are going to practice applying the skills and concepts we learned while building Fantasy Quest.
 
-Assignment
-Complete the process_transactions function. It takes a list of purchase orders. Each order is a dictionary. Look through the main() function to see the shape of this data.
+Number Sum
+Write a function called number_sum(n) that adds up all the numbers from 1 to n. For example:
 
-First, create an empty list of leftovers.
+number_sum(5) -> 1+2+3+4+5 -> 15
 
-Then, loop over the list of purchase orders.
-
-For each order in the provided list, try to process the order with the purchase_item function. If an exception is raised, catch it and print the error message.
-
-If the purchase was successful. Append the remaining money to the leftovers list. If there is an error, do not add anything to the leftovers.
-
-Be sure to loop over the entire list of purchase orders.
-
-Finally, after the loop has ended, return the leftovers list.
-
-You must keep the same order of purchases, but with the unsuccessful purchases removed.
-
-Remember to remove any extra print statements you added for debugging before submitting the lesson!
+number_sum(3) -> 1+2+3 -> 6
 """
-def purchase_item(price, gold_available):
-    if gold_available < price:
-        raise Exception("not enough gold")
-    return gold_available - price
 
-def process_transactions(purchase_orders):
-    leftovers = []
-    for order in purchase_orders:
-        try:
-            leftover = purchase_item(order["price"], order["gold_available"])
-            leftovers.append(leftover)
-        except Exception as e:
-            print(e)
-    return leftovers
+def number_sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
 
-# Don't edit below this line
-
-
-def main():
-    print("Processing transactions...")
-    leftovers = process_transactions(
-        [
-            {"price": 10.00, "gold_available": 125.00},
-            {"price": 5.00, "gold_available": 2.00},
-            {"price": 20.01, "gold_available": 5.20},
-            {"price": 1.04, "gold_available": 254.00},
-            {"price": 40.20, "gold_available": 6.00},
-            {"price": 16.00, "gold_available": 235.01},
-            {"price": 10.70, "gold_available": 10.70},
-            {"price": 12.00, "gold_available": 2.30},
-        ]
-    )
-    print("Transactions complete!")
-    print("Leftover amounts for successful purchases:")
-    for leftover in leftovers:
-        print(f" * {leftover:.2f}")
-
-main()
+    return sum
