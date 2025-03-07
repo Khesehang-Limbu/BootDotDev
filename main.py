7e17d19b9c9da4585cdf0c6c441e0e56111d38e2
KhesehangLimbu <khesehang81@gmail.com>
2025-03-07 17:03:29 +0545

    Abstraction: Deck of Cards

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
 """
 
-The Calculator
+Deck of Cards
 Assignment
-Complete the Calculator class.
+Finish the DeckOfCards class. The SUITS and RANKS of each card have been provided for you as class variables. You won't need to modify them, but you will need to use them.
 
 Constructor
-Create a private instance variable called result initialized to 0.
+Initialize a private empty list called cards.
+Fill that empty list by calling the create_deck method within the constructor.
+create_deck(self)
+This method should create a (Rank, Suit) tuple for all 52 cards in the deck and append them to the cards list.
 
-Math
-The following methods should perform their respective mathematic computations. The "left-hand side" of each operation should be the current value of the result variable. The "right-hand side" of each operation will be the value passed in.
+Order matters! The cards should be appended to the list in the following order: all ranks of hearts, then diamonds, then clubs, and finally spades. Within each suit, the cards should be ordered from lowest rank (Ace) to highest rank (King).
 
-add(self, a)
-subtract(self, a)
-multiply(self, a)
-divide(self, a): If the user attempts to divide by 0, raise a ValueError with "cannot divide by zero" as the argument
-modulo(self, a): If the user attempts to divide by 0, raise a ValueError with "cannot divide by zero" as the argument
-power(self, a):
-square_root(self)
-Helper Methods
-clear(self): reset the result variable to 0
-get_result(self): return the current value stored in the calculator's private result variable.
+shuffle_deck(self)
+This method should use the random.shuffle() method (available from the random package) to shuffle the cards in the deck.
+
+deal_card(self)
+This method should .pop() the first card off the top of the deck (top of the deck is the end of the list) and return it. If there are no cards left in the deck the method should instead return None.
 
 """
 
-class Calculator:
+import random
+
+
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
     def __init__(self):
-        self.__result = 0
+        self.__cards = []
+        self.create_deck()
 
-    def add(self, a):
-        self.__result += a
+    def create_deck(self):
+        for suit in DeckOfCards.SUITS:
+            for rank in DeckOfCards.RANKS:
+                self.__cards.append((rank, suit))
 
-    def subtract(self, a):
-        self.__result -= a
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
 
-    def multiply(self, a):
-        self.__result *= a
+    def deal_card(self):
+        if len(self.__cards) == 0:
+            return None
+        last_card = self.__cards.pop()
+        return last_card
 
-    def divide(self, a):
-        if a == 0:
-            raise ValueError("cannot divide by zero")
-        self.__result /= a
+    # don't touch below this line
 
-    def modulo(self, a):
-        if a == 0:
-            raise ValueError("cannot divide by zero")
-        self.__result %= a
-
-    def power(self, a):
-        exponent = self.__result
-        for _ in range(1, a):
-            self.__result *= exponent
-
-    def square_root(self):
-        for i in range(2, self.__result):
-            if self.__result % i == 0 and i * i == self.__result:
-                self.__result = i
-                return
-
-    def clear(self):
-        self.__result = 0
-
-    def get_result(self):
-        return self.__result
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
