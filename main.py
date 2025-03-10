124a2a4e7242ee68b0bf87dcb6368098222e186c
KhesehangLimbu <khesehang81@gmail.com>
2025-03-10 14:40:40 +0545

    Polymorphism: Overloading Built-In Methods

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 """
 
-Operator Overloading
-Another kind of built-in polymorphism in Python is the ability to override how an operator works. For example, the + operator works for built-in types like integers and strings.
-
-print(3 + 4)
-# prints "7"
-
-print("three " + "four")
-# prints "three four"
-
-Custom classes on the other hand don't have any built-in support for those operators:
+Overriding Built-In Methods
+Last but not least, let's take a look at some of the built-in methods we can override in Python. While there isn't a default behavior for the arithmetic operators like we just saw, there is a default behavior for printing a class.
 
 class Point:
     def __init__(self, x, y):
@@ -18,57 +10,37 @@ class Point:
 
 
 p1 = Point(4, 5)
-p2 = Point(2, 3)
-p3 = p1 + p2
-# TypeError: unsupported operand type(s) for +: 'Point' and 'Point'
+print(p1)
+# prints "<Point object at 0xa0acf8>"
 
-However, we can add our own support! If we create an __add__(self, other) method on our class, the Python interpreter will use it when instances of the class are being added with the + operator. Here's an example:
+That's not super useful! Let's teach instances of our Point object to print themselves. The __str__ method (short for "string") lets us do just that. It takes no inputs but returns a string that will be printed to the console when someone passes an instance of the class to Python's print() function.
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def __add__(self, point):
-        x = self.x + point.x
-        y = self.y + point.y
-        return Point(x, y)
+    def __str__(self):
+        return f"({self.x},{self.y})"
 
 p1 = Point(4, 5)
-p2 = Point(2, 3)
-p3 = p1 + p2
-# p3 is (6, 8)
+print(p1)
+# prints "(4,5)"
 
-Now, when p1 + p2 is executed, under the hood the Python interpreter just calls p1.__add__(p2).
+Note: the __repr__ method works in a similar way, you'll see it from time to time.
 
 Assignment
-In Age of Dragons, players can upgrade their weaponry. To make "crafting" simple for other developers, we'll use operator overloading on the Sword class. Note how the test suite attempts to use the + operator to craft the swords. Overload the + operator to craft the swords.
+Dragons are egotistical creatures, let's give them a great format for announcing their presence in "Age of Dragons". When print() is called on an instance of a Dragon, the string I am {0}, the {1} dragon should be printed.
 
-Create an __add__(self, other) method on the Sword class. It will be used to "craft" two swords together to create a new sword. Return a new Sword instance. sword_type is just a string, one of:
-
-bronze
-
-iron
-
-steel
-
-If two "bronze" swords are crafted together, return a new "iron" sword.
-
-If two "iron" swords are crafted together, return a new "steel" sword.
-
-If a player tries to craft anything other than 2 bronze swords or 2 iron swords, just raise an Exception with the message "cannot craft".
+{0} is the name of the dragon.
+{1} is the color of the dragon.
 
 """
 
-class Sword:
-    def __init__(self, sword_type):
-        self.sword_type = sword_type
+class Dragon:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
 
-    def __add__(self, other):
-        if self.sword_type == other.sword_type and self.sword_type == "bronze":
-            return Sword("iron")
-        elif self.sword_type == other.sword_type and self.sword_type == "iron":
-            return Sword("steel")
-        else:
-            raise Exception("cannot craft")
-
+    def __str__(self):
+        return f"I am {self.name}, the {self.color} dragon"
