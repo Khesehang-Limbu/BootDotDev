cdee92522bc016d1b414cec659a8f06dbdfe61c9
KhesehangLimbu <khesehang81@gmail.com>
2025-03-11 15:46:07 +0545

    Functional Programming: Filter

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
 """
 
 Map
-"Map", "filter", and "reduce" are three commonly used higher-order functions in functional programming.
+Filter
+The built-in filter function takes a function and an iterable (in this case a list) and returns an iterator that only contains elements from the original iterable where the result of the function on that item returned True.
 
-In Python, the built-in map function takes a function and an iterable (in this case a list) as inputs. It returns an iterator that applies the function to every item, yielding the results.
+filter function
 
-map function
+In Python:
 
-With map, we can operate on lists without using loops and nasty stateful variables. For example:
+def is_even(x):
+    return x % 2 == 0
 
-def square(x):
-    return x * x
-
-nums = [1, 2, 3, 4, 5]
-squared_nums = map(square, nums)
-print(list(squared_nums))
-# [1, 4, 9, 16, 25]
-
-The list type constructor, list() converts the map object back into a standard list.
+numbers = [1, 2, 3, 4, 5, 6]
+evens = list(filter(is_even, numbers))
+print(evens)
+# [2, 4, 6]
 
 Assignment
-Markdown supports two different styles of bullet points, - and *. We prefer *, so, we need a function to convert any - bullet points to * bullet points.
+Complete the remove_invalid_lines function. It accepts a document as input. It should:
 
-Complete the change_bullet_style function. It takes a document (a string) as input, and returns a single string as output. The returned string should have any lines that start with a - character replaced with a * character.
-
+Use the built-in filter function and a lambda to return a copy of the input document
+Remove any lines that start with a - character.
+Keep all other lines and preserve trailing newlines.
 For example, this:
 
-- This is a bullet
-- This is a bullet
+* Star Wars episode 1 is underrated
+- Star Wars episode 9 is fine
+* Star Wars episode 3 is the best
 
-Becomes:
 
-* This is a bullet
-* This is a bullet
+Should become:
 
-Use the built-in map function to apply the provided convert_line function to each line of the input string. Use .split() and .join() to split the document into a list of lines, and then join the lines back together. This should preserve the original line breaks. Don't use the .replace() string method.
-
-Examples of split and join:
-
-# my_document is a string with newlines
-lines_list = my_document.split("\n")
-
-rejoined_doc = "\n".join(lines_list)
+* Star Wars episode 1 is underrated
+* Star Wars episode 3 is the best
 
 """
 
 
-def change_bullet_style(document):
-    return '\n'.join(map(convert_line, document.split("\n")))
+def remove_invalid_lines(document):
+    print(document.split("\n"))
+    return '\n'.join(filter(is_invalid_line, document.split("\n")))
 
 
-# Don't edit below this line
-
-
-def convert_line(line):
-    old_bullet = "-"
-    new_bullet = "*"
-    if len(line) > 0 and line[0] == old_bullet:
-        return new_bullet + line[1:]
-    return line
+def is_invalid_line(line):
+    if line == "":
+        return True
+    if line[0] != "-":
+        return True
+    return False
