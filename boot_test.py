3175990c576e7b581a924f350bb30b89aaed6f2a
KhesehangLimbu <khesehang81@gmail.com>
2025-03-12 16:53:44 +0545

    Functional Programming: Closures

--- boot_test.py
+++ boot_test.py
@@ -1,65 +1,50 @@
 from main import *
 
-
 run_cases = [
     (
-        "hello world. this is a test.",
-        ["--replace", "--capitalize"],
-        [("hello", "hi"), ("world", "earth"), ("test", "test case")],
-        "Hi earth. This is a test case.",
-    ),
-    (
-        "hello world. this is a test.",
-        ["--capitalize", "--uppercase"],
-        [("world", "earth"), ("test", "test case")],
-        "Hello WORLD. This is a TEST.",
-    ),
-    (
-        "the quick brown fox jumps over the lazy dog.",
-        [],
-        [],
-        "missing options",
-    ),
+        [
+            "Welcome to the jungle",
+            "We've got fun and games",
+            "We've got everything you want honey",
+        ],
+        15,
+    )
 ]
-
 
 submit_cases = run_cases + [
     (
-        "the quick brown fox jumps over the lazy dog.",
-        ["--replace", "--lowercase"],
-        [],
-        "invalid option",
+        [
+            "We are the champions my friends",
+            "And we'll keep on fighting till the end",
+        ],
+        14,
     ),
     (
-        "the quick brown fox jumps over the lazy dog.",
-        ["--remove", "--capitalize"],
-        [("quick", "slow"), ("fox", "wolf"), ("lazy", "active")],
-        "The  brown  jumps over the  dog.",
-    ),
-    (
-        "the quick brown fox jumps over the lazy dog",
-        ["--replace", "--remove", "--uppercase"],
-        [],
-        "the quick brown fox jumps over the lazy dog",
+        [
+            "I've got another confession to make",
+            "I'm your fool",
+            "Everyone's got their chains to break",
+            "Holdin' you",
+        ],
+        17,
     ),
 ]
 
 
-def test(filter_cmd, content, options, word_pairs, expected_output):
+def test(inputs, expected_output):
     print("---------------------------------")
-    print(f"Content: {content}")
-    print("Options:")
-    for option in options:
-        print(option)
-    print("Word Pairs:")
-    for word in word_pairs:
-        print(word)
-    try:
-        result = filter_cmd(content, options, word_pairs)
-    except Exception as e:
-        result = str(e)
+    print(f"Input:")
+    for x in inputs:
+        print(f" * {x}")
     print(f"Expecting: {expected_output}")
-    print(f"   Actual: {result}")
+    aggregator = word_count_aggregator()
+
+    try:
+        for input in inputs:
+            result = aggregator(input)
+    except Exception as e:
+        result = e
+    print(f"Actual: {result}")
     if result == expected_output:
         print("Pass")
         return True
@@ -70,13 +55,9 @@ def test(filter_cmd, content, options, word_pairs, expected_output):
 def main():
     passed = 0
     failed = 0
-    print("Filters:")
-    for k, v in filters.items():
-        print(f"* ({k}, {v.__name__})")
-    filter_cmd = get_filter_cmd(filters)
     skipped = len(submit_cases) - len(test_cases)
     for test_case in test_cases:
-        correct = test(filter_cmd, *test_case)
+        correct = test(*test_case)
         if correct:
             passed += 1
         else:
--- main.py
+++ main.py
@@ -1,61 +1,57 @@
 """
 
-Upgrade Filter Command
-Users like the filter command, but think it could be better. They want access to more than two filter options and to customize the words that get filtered. It's time to upgrade the filter command feature.
+Closures
+A closure is a function that references variables from outside its own function body. The function definition and its environment are bundled together into a single entity.
+
+Put simply, a closure is just a function that keeps track of some values from the place where it was defined, no matter where it is executed later on.
+
+Example
+The concatter() function returns a function called doc_builder (yay higher-order functions!) that has a reference to an enclosed doc value.
+
+def concatter():
+	doc = ""
+	def doc_builder(word):
+		# "nonlocal" tells Python to use the 'doc'
+		# variable from the enclosing scope
+		nonlocal doc
+		doc += word + " "
+		return doc
+	return doc_builder
+
+# save the returned 'doc_builder' function
+# to the new function 'harry_potter_aggregator'
+harry_potter_aggregator = concatter()
+harry_potter_aggregator("Mr.")
+harry_potter_aggregator("and")
+harry_potter_aggregator("Mrs.")
+harry_potter_aggregator("Dursley")
+harry_potter_aggregator("of")
+harry_potter_aggregator("number")
+harry_potter_aggregator("four,")
+harry_potter_aggregator("Privet")
+
+print(harry_potter_aggregator("Drive"))
+# Mr. and Mrs. Dursley of number four, Privet Drive
+
+When concatter() is called, it creates a new "stateful" function that remembers the value of its internal doc variable. Each successive call to harry_potter_aggregator appends to that same doc.
+
+nonlocal
+Python has a keyword called nonlocal that's required to modify a variable from an enclosing scope. Most programming languages don't require this keyword, but Python does.
 
 Assignment
-Complete the get_filter_cmd function. It should take a dictionary as input, filters, and return a function, filter_cmd.
-filters contains option string and filter function key/value pairs.
-filter_cmd should take as input a string content to be filtered, a list of strings options, and a list of tuples word_pairs.
-The filter_cmd should filter and return the content, filtered according to the input options
-If there are no options in the options list, raise an exception "missing options".
-For each option, if its option string is in the filters dictionary, then filter the content by passing the content and word_pairs to the option's filter.
-Raise an exception "invalid option" for any option that is passed but not found in the filters dictionary.
+Doc2Doc keeps track of how many words are in a collection of documents.
+
+Complete the word_count_aggregator function. It should return a function that calculates the number of words in its input (doc, a string). It should then add that number to an enclosed count value and return the new count. In other words, it keeps a running total of the count variable within a closure.
 
 """
 
-
-def get_filter_cmd(filters):
-    def filter_cmd(content, options, word_pairs):
-        if len(options) == 0:
-            raise Exception("missing options")
-        for option in options:
-            try:
-                content = filters[option](content, word_pairs)
-            except Exception:
-                raise Exception("invalid option")
-        return content
-    return filter_cmd
+import functools
 
 
-# don't touch below this line
-
-
-def replace_words(content, word_pairs):
-    for pair in word_pairs:
-        content = content.replace(pair[0], pair[1])
-    return content
-
-
-def remove_words(content, word_pairs):
-    for pair in word_pairs:
-        content = content.replace(pair[0], "")
-    return content
-
-
-def capitalize_sentences(content, word_pairs):
-    return ". ".join(map(str.capitalize, content.split(". ")))
-
-
-def uppercase_words(content, word_pairs):
-    for pair in word_pairs:
-        content = content.replace(pair[0], pair[0].upper())
-    return content
-
-
-filters = {
-    "--replace": replace_words,
-    "--remove": remove_words,
-    "--capitalize": capitalize_sentences,
-    "--uppercase": uppercase_words,
-}
+def word_count_aggregator():
+    total_words = 0
+    def calculate_words(doc):
+        nonlocal total_words
+        total_words += len(doc.split(" "))
+        return total_words
+    return calculate_words
