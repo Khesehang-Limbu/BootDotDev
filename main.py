69bda678be5e18b96d524ee888620c5bc168c7a7
KhesehangLimbu <khesehang81@gmail.com>
2025-03-13 01:22:53 +0545

    Functional Programming: Enums

--- main.py
+++ main.py
@@ -1,91 +1,40 @@
 """
 
-Sum Types
-Remember when I said, "Pure functions are my favorite part of functional programming"? Well, sum types are a close second.
+Enums
+Doing the admittedly weird class and isinstance() thing works, but it turns out, there's a better way in some cases. If you're trying to represent a fixed set of values (but not store additional data within them) enums are the way to go.
 
-A "sum" type is the opposite of a "product" type. This Python object is an example of a product type:
 
-man.studies_finance = True
-man.has_trust_fund = False
+Let's say we have a Color variable that we want to restrict to only three possible values:
 
-The total number of combinations a man can have is 4, the product of 2 * 2:
+RED
+GREEN
+BLUE
+We could use a plain-old string to represent these values, but that's annoying because we have to remember all the "valid" values and defensively check for invalid ones all over our codebase. Instead, we can use an Enum:
 
-studies_finance	has_trust_fund
-True	True
-True	False
-False	True
-False	False
-If we add a third attribute, perhaps a has_blue_eyes boolean, the total number of possibilities multiplies again, to 8!
+from enum import Enum
 
-studies_finance	has_trust_fund	has_blue_eyes
-True	True	True
-True	True	False
-True	False	True
-True	False	False
-False	True	True
-False	True	False
-False	False	True
-False	False	False
-But let's pretend that we live in a world where there are really only three types of people that our program cares about:
+Color = Enum('Color', ['RED', 'GREEN', 'BLUE'])
+print(Color.RED)  # this works, prints 'Color.RED'
+print(Color.TEAL) # this raises an exception
 
-Dateable
-Undateable
-Maybe dateable
-We can reduce the number of cases our code needs to handle by using a (admittedly fake Pythonic) sum type with only 3 possible types:
+Now Color is a sum type! At least, as close as we can get in Python.
 
-class Person:
-    def __init__(self, name):
-        self.name = name
+There are a few benefits:
 
-class Dateable(Person):
-    pass
-
-class MaybeDateable(Person):
-    pass
-
-class Undateable(Person):
-    pass
-
-Then we can use the isinstance built-in function to check if a Person is an instance of one of the subclasses. It's a clunky way to represent sum types, but hey, it's Python.
-
-def respond_to_text(guy_at_bar):
-    if isinstance(guy_at_bar, Dateable):
-        return f"Hey {guy_at_bar.name}, I'd love to go out with you!"
-    elif isinstance(guy_at_bar, MaybeDateable):
-        return f"Hey {guy_at_bar.name}, I'm busy but let's hang out sometime later."
-    elif isinstance(guy_at_bar, Undateable):
-        return "Have you tried being rich?"
-    else:
-        raise ValueError("invalid person type")
-
-Sum Types
-As opposed to product types, which can have many (often infinite) combinations, sum types have a fixed number of possible values. To be clear: Python doesn't really support sum types. We have to use a workaround and invent our own little system and enforce it ourselves.
-
+A "Color" can only be RED, GREEN, or BLUE. If you try to use Color.TEAL, Python raises an exception.
+There is a central place to see the "valid" values for a Color.
+Each "Color" has a "name" (e.g. Color.RED) and a "value" (e.g. 1). The value is an integer and is used under the hood instead of the name. Integers take up less memory than strings, which helps with performance.
 Assignment
-Whenever a document is parsed by Doc2Doc, it can either succeed or fail. In functional programming, we often represent errors as data (e.g. the ParseError class) rather than by raiseing exceptions, because exceptions are side effects. (This isn't standard Python practice, but it's useful to understand from an FP perspective)
+Create an Enum called Doctype with values:
 
-Complete the Parsed and ParseError subclasses.
-
-Parsed represents success. It should accept a doc_name string and a text string and save them as properties of the same name.
-ParseError represents failure. It should accept a doc_name string and an err string and save them as properties of the same name.
-The test suite uses the isinstance function to see if an error occurred based on the class type.
+PDF
+TXT
+DOCX
+MD
+HTML
 
 """
 
-class MaybeParsed:
-    pass
+from enum import Enum
 
-
-# don't touch above this line
-
-
-class Parsed(MaybeParsed):
-    def __init__(self, doc_name, text):
-        self.doc_name = doc_name
-        self.text = text
-
-
-class ParseError(MaybeParsed):
-    def __init__(self, doc_name, err):
-        self.doc_name = doc_name
-        self.err = err
+Doctype = Enum("Doctype", ["PDF", "TXT", "DOCX", "MD", "HTML"])
