72c41d72a39e4553f630fe519a747a0284f7082f
KhesehangLimbu <khesehang81@gmail.com>
2025-03-12 23:28:59 +0545

    Functional Programming: HTML Table

--- boot_test.py
+++ boot_test.py
@@ -1,56 +1,36 @@
 from main import *
 
-
 run_cases = [
     (
-        "#",
-        3,
-        """###
-@##
-$$$
-###""",
-        2,
-    ),
-    (
-        "$",
-        2,
-        """$$$
-$
-***
-@@@
-$$
-$$$""",
-        3,
+        [
+            ["Scooby Doo", "Lassie"],
+            ["Blue", "Wishbone"],
+        ],
+        ["Cartoon TV Dogs", "Real TV Dogs"],
+        "<table><tr><th>Cartoon TV Dogs</th><th>Real TV Dogs</th></tr><tr><td>Scooby Doo</td><td>Lassie</td></tr><tr><td>Blue</td><td>Wishbone</td></tr></table>",
     ),
 ]
 
 submit_cases = run_cases + [
-    ("%", 1, "", 0),
     (
-        "*",
-        3,
-        """***
-*
-$$$$$$
-xxx
-****
-***
-***""",
-        4,
+        [
+            ["Garfield", "Salem"],
+            ["Tom", "Mr. Bigglesworth"],
+        ],
+        ["Cartoon TV Cats", "Real TV Cats"],
+        "<table><tr><th>Cartoon TV Cats</th><th>Real TV Cats</th></tr><tr><td>Garfield</td><td>Salem</td></tr><tr><td>Tom</td><td>Mr. Bigglesworth</td></tr></table>",
     ),
 ]
 
 
-def test(char, length, doc, expected_output):
+def test(data_rows, headers, expected_output):
     print("---------------------------------")
-    print(f"Input char: {char}")
-    print(f"Input length: {length}")
-    print(f"Input doc:")
-    print(doc)
-    print(f"Expecting: {expected_output}")
-    num_lines = lines_with_sequence(char)(length)(doc)
-    print(f"Actual: {num_lines}")
-    if num_lines == expected_output:
+    print(f"Data Rows: {data_rows}")
+    print(f"Headers: {headers}")
+    print(f"Expecting:\n{expected_output}")
+    result = create_html_table(data_rows)(headers)
+    print(f"Actual:\n{result}")
+    if result == expected_output:
         print("Pass")
         return True
     print("Fail")
