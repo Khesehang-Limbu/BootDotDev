7552f78c02e7dc2fdaeb4d2a243db166b3adcb7c
KhesehangLimbu <khesehang81@gmail.com>
2025-03-12 11:49:51 +0545

    Functional Programming: Recursion on a Tree

--- main.py
+++ main.py
@@ -1,65 +1,75 @@
 """
 
-Nested Sum
-Recursion is hard for all new developers. If you're struggling, that's okay! Take your time. That's why we're doing a few extra practice problems.
+Recursion on a Tree
+Recursion is often used in "tree-like" structures. For example:
+
+Nested dictionaries
+File systems
+HTML documents
+JSON objects
+That's because trees can have unknown depth. It's hard to write a series of loops because you don't know how many levels deep the tree goes.
+
+for entry_i in directory:
+    if entry_i.is_dir:
+        for entry_j in entry_i:
+            if entry_j.is_dir:
+                for entry_k in entry_j:
+                    ...
 
 Assignment
-In Doc2Doc, users can process files or entire directories. We need to know the total size of those files and directories (measured in bytes).
+You're responsible for a module in Doc2Doc that can scan a file system (represented in our code as nested dictionaries) and create a list of the filenames.
 
-Due to the nested nature of directories, we represent a root directory as a list of lists. Each list represents a directory, and each number represents the size of a file in that directory. For example, here's a directory that contains 2 files at the root level, then a nested directory with its own two files:
+Complete the recursive list_files function. It accepts two arguments:
 
-root = [
-    1,
-    2,
-    [3, 4]
+parent_directory: A dictionary of dictionaries representing the current directory. A child directory's value is a dictionary and a file's value is None.
+current_filepath: A string representing the current path (e.g. /dir1/dir2/filename.txt)
+It should return a list of all filepaths in the parent_directory.
+
+Steps
+Create an empty list to store the file paths.
+Use a for-loop to iterate through the keys of the parent_directory dictionary:
+Use the key to create a new file path by concatenating a slash / and the key to the end of the current_filepath.
+If the value is None, the key is a filename. .append() the new file path to the list of file paths.
+Otherwise, the value is a child directory dictionary. Recursively call list_files with the child directory dictionary and the new file path.
+Use .extend() to add the results of the recursive call to the list of file paths.
+Return the list of file paths.
+Example parent_directory:
+
+{
+    "Documents": {
+        "Proposal.docx": None,
+        "Receipts": {
+            "January": {
+                "receipt1.txt": None,
+                "receipt2.txt": None
+            },
+            "February": {
+                "receipt3.txt": None
+            }
+        }
+    },
+}
+
+Resulting list of file paths:
+
+[
+    "/Documents/Proposal.docx",
+    "/Documents/Receipts/January/receipt1.txt",
+    "/Documents/Receipts/January/receipt2.txt",
+    "/Documents/Receipts/February/receipt3.txt"
 ]
-print(sum_nested_list(root))
-# 10
-
-Here's a more complex example:
-
-root
-├── scripts.txt (5 bytes)
-├── characters (dir)
-│   ├── zuko.txt (6 bytes)
-│   └── aang.txt (7 bytes)
-└── seasons (dir)
-    ├── season1 (dir)
-    │   ├── the_avatar_returns.docx (8 bytes)
-    │   └── the_southern_air_temple.docx (9 bytes)
-    └── season2_notes.txt (10 bytes)
-
-
-Which would be represented as:
-
-root = [
-    5,
-    [6, 7],
-    [[8, 9], 10]
-]
-print(sum_nested_list(root))
-# 45
-
-Complete the sum_nested_list function. It takes a nested list of integers as input and should return the total size of all files in the list. It's a recursive function.
-
-Here's some pseudocode to help you get started:
-
-Create an integer variable to keep track of the total size.
-For each item in the list (use a loop here):
-If the item is an integer, add it to the total size.
-If the item is a list, use a recursive call to sum_nested_list to get the size of that list. Add that size to the total size.
-Return the total size when you're done iterating.
 
 """
 
 
-def sum_nested_list(lst):
-    total_size = 0
+def list_files(parent_directory, current_filepath=""):
+    current_list = []
 
-    for item in lst:
-        if isinstance(item, int):
-            total_size += item
-        elif isinstance(item, list):
-            total_size += sum_nested_list(item)
-    return total_size
+    for key in parent_directory.keys():
+        new_file_path = f"{current_filepath}/{key}"
+        if parent_directory[key] is None:
+            current_list.append(new_file_path)
+        else:
+            current_list.extend(list_files(parent_directory[key], new_file_path))
 
+    return current_list
