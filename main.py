8cde98a12ea6cb9efd39da2e88285f41209b388c
KhesehangLimbu <khesehang81@gmail.com>
2025-03-12 16:57:44 +0545

    Functional Programming: Closure Practice

--- main.py
+++ main.py
@@ -1,57 +1,39 @@
 """
 
-Closures
-A closure is a function that references variables from outside its own function body. The function definition and its environment are bundled together into a single entity.
+Closure Practice
+Remember, a closure is a function that retains the state of its environment. That makes it useful for tracking data as it changes over time, but it can come at the cost of understandability.
 
-Put simply, a closure is just a function that keeps track of some values from the place where it was defined, no matter where it is executed later on.
+When not to use the nonlocal keyword: when the variable is mutable (such as a list, dictionary or set), and you are modifying its contents rather than reassigning the variable. You only need the nonlocal keyword if you are reassigning a variable instead of modifying its contents (which you must do to change immutable values such as strings and integers).
 
-Example
-The concatter() function returns a function called doc_builder (yay higher-order functions!) that has a reference to an enclosed doc value.
-
-def concatter():
-	doc = ""
-	def doc_builder(word):
-		# "nonlocal" tells Python to use the 'doc'
-		# variable from the enclosing scope
-		nonlocal doc
-		doc += word + " "
-		return doc
-	return doc_builder
-
-# save the returned 'doc_builder' function
-# to the new function 'harry_potter_aggregator'
-harry_potter_aggregator = concatter()
-harry_potter_aggregator("Mr.")
-harry_potter_aggregator("and")
-harry_potter_aggregator("Mrs.")
-harry_potter_aggregator("Dursley")
-harry_potter_aggregator("of")
-harry_potter_aggregator("number")
-harry_potter_aggregator("four,")
-harry_potter_aggregator("Privet")
-
-print(harry_potter_aggregator("Drive"))
-# Mr. and Mrs. Dursley of number four, Privet Drive
-
-When concatter() is called, it creates a new "stateful" function that remembers the value of its internal doc variable. Each successive call to harry_potter_aggregator appends to that same doc.
-
-nonlocal
-Python has a keyword called nonlocal that's required to modify a variable from an enclosing scope. Most programming languages don't require this keyword, but Python does.
+Let's try a closure without the nonlocal keyword.
 
 Assignment
-Doc2Doc keeps track of how many words are in a collection of documents.
+Complete the new_collection function.
 
-Complete the word_count_aggregator function. It should return a function that calculates the number of words in its input (doc, a string). It should then add that number to an enclosed count value and return the new count. In other words, it keeps a running total of the count variable within a closure.
+It accepts a list of strings, initial_docs.
+
+It should return a function that closes over a copy of initial_docs. This function should accept a string, append that string to the closed-over list, and return the new list.
+
+Do not modify the original initial_docs list!
+
+Example Usage
+new_collection = new_collection(["doc1", "doc2", "doc3"])
+print(new_collection("doc4"))
+# ['doc1', 'doc2', 'doc3', 'doc4']
+print(new_collection("doc5"))
+# ['doc1', 'doc2', 'doc3', 'doc4', 'doc5']
 
 """
 
 import functools
 
 
-def word_count_aggregator():
-    total_words = 0
-    def calculate_words(doc):
-        nonlocal total_words
-        total_words += len(doc.split(" "))
-        return total_words
-    return calculate_words
+def new_collection(initial_docs):
+    new_list = initial_docs.copy()
+
+    def append_item_to_list(item):
+        new_list.append(item)
+        return new_list
+    return append_item_to_list
+
+
