3e591bdee55ca7b68d44bf26a45f59cacb646898
KhesehangLimbu <khesehang81@gmail.com>
2025-03-12 15:11:11 +0545

    Functional Programming: More Transformations

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 """
 
-Function Transformations
-"Function transformation" is just a more concise way to describe a specific type of higher order function. It's when a function takes a function (or functions) as input and returns a new function. Let's look at an example:
-
-higher order function
+More Transformations
+Here's some example code for you to reference as you work through the assignment:
 
 def multiply(x, y):
     return x * y
@@ -11,9 +9,6 @@ def multiply(x, y):
 def add(x, y):
     return x + y
 
-# self_math is a higher order function
-# input: a function that takes two arguments and returns a value
-# output: a new function that takes one argument and returns a value
 def self_math(math_func):
     def inner_func(x):
         return math_func(x, x)
@@ -28,60 +23,33 @@ print(square_func(5))
 print(double_func(5))
 # prints 10
 
-The self_math function takes a function that operates on two different parameters (e.g. multiply or add) and returns a new function that operates on one parameter twice (e.g. square or double).
-
 Assignment
-Doc2Doc needs a good logging system so that users and developers alike can see what's going on under the hood. Complete the get_logger function.
+Complete the doc_format_checker_and_converter function.
 
-It takes a formatter function as a parameter and returns a new function. Steps:
+It takes a conversion_function and a list of valid_formats as parameters. It should return a new function that takes two parameters of its own:
 
-Define a new function, logger, inside get_logger (see self_math above as an example). It accepts two strings. You can just name them first and second if you like.
-The logger function should not return anything. It should simply print the result of calling the given formatter function with the first and second strings as arguments.
-Return the new logger function for the test suite to use.
+filename: The name of the file to be converted
+content: The content (body text) of the file to be converted
+If the file extension of the filename is in the valid_formats list, then it should return the result of calling the conversion_function on the content. Otherwise, it should raise a ValueError with the message invalid file format.
 
 """
 
 
-def get_logger(formatter):
-    # ?
-    def logger(first, second):
-        print(formatter(first, second))
-    return logger
+def doc_format_checker_and_converter(conversion_function, valid_formats):
+    def inner_func(filename, content):
+        if filename.split(".")[-1] in valid_formats:
+            return conversion_function(content)
+        else:
+            raise Exception("invalid file format")
+    return inner_func
 
 
 # Don't edit below this line
 
 
-def test(first, errors, formatter):
-    print("Logs:")
-    logger = get_logger(formatter)
-    for err in errors:
-        logger(first, err)
-    print("====================================")
+def capitalize_content(content):
+    return content.upper()
 
 
-def colon_delimit(first, second):
-    return f"{first}: {second}"
-
-
-def dash_delimit(first, second):
-    return f"{first} - {second}"
-
-
-def main():
-    db_errors = [
-        "out of memory",
-        "cpu is pegged",
-        "networking issue",
-        "invalid syntax",
-    ]
-    test("Doc2Doc FATAL", db_errors, colon_delimit)
-
-    mail_errors = [
-        "email too large",
-        "non alphanumeric symbols found",
-    ]
-    test("Doc2Doc WARNING", mail_errors, dash_delimit)
-
-
-main()
+def reverse_content(content):
+    return content[::-1]
